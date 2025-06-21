eca134e01448db4b3277c3c11e41bb07cda5601b
Rohit Kumar <rk142003mark@gmail.com>
2025-06-21 14:34:28 +0530

    uplaod resolve

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "upload/");
+    cb(null, "backend/upload/");
   },
 
   filename: (req, file, cb) => {
