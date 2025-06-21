9ec62eb5e1a292e0d6ecd311d25670ba52233fb0
Rohit Kumar <rk142003mark@gmail.com>
2025-06-21 13:24:39 +0530

    add

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,8 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://movies-hub-75ol.onrender.com";
 export const USERS_URL = "/api/v1/users";
 export const GENRE_URL = "/api/v1/genre";
 export const MOVIE_URL = "/api/v1/movies";
 export const UPLOAD_URL = "/api/v1/upload";
+
+// live base : https://movies-hub-75ol.onrender.com
+// local: http://localhost:8000
