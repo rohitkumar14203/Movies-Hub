1c00a0009bdad4e6ea9a01d52b953c2be3472a3c
Rohit Kumar <rk142003mark@gmail.com>
2025-06-21 19:41:06 +0530

    uplaod resolve

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -40,7 +40,7 @@ router.post("/", (req, res) => {
     } else if (req.file) {
       res.status(200).send({
         message: "Image uploaded successfully",
-        image: `/upload/${req.file.path}`,
+        image: `${req.file.path}`,
       });
     } else {
       res.status(400).send({ message: "No image file provided" });
