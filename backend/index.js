4d4a7e25d8e7e3d4efc4ece98d9a60d3c20f041d
Rohit Kumar <rk142003mark@gmail.com>
2025-06-21 14:04:34 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://movies-hub-navy-omega.vercel.app", //http://localhost:5173
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
